553d4589addb98f6a806d938fa0774cf166eb2c1
artemov <artik3314@gmail.com>
2023-11-22 21:27:30 +0300

    bootstrap and md files

--- RazorPagesApp.Sample/Pages/Index.cshtml.cs
+++ RazorPagesApp.Sample/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Routing;
 
@@ -9,15 +10,26 @@ namespace SampleApp.RazorPage.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        [BindProperty(SupportsGet = true)]
+        public string Param { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Number { get; set; }
+
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
         // On{verb}[handler][Async]
-        public void OnGet()
+        public void OnGet(string param)
         {
-
+            _logger.LogInformation($"Параметр метода:{param}");
+            _logger.LogInformation($"Привязанное свойство Param:{Param}");
+            _logger.LogInformation($"Значение маршрута Number:{Number}");
+            _logger.LogInformation($"Модель состояния ModelState:{ModelState.IsValid}");
+            _logger.LogInformation($"Request Path:{HttpContext.Request.Path.Value}");
         }
 
         /* https://localhost:7246/?handler=SayHello как параметр строки запроса */
